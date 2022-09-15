fb7487b4ba689c94f9a8713e8a232d34ca153a8a
samolajide23 <d00231093@student.dkit.ie>
2022-09-15 17:24:32 +0100

    Creating Board

---
+++ pongGame.py
@@ -0,0 +1,35 @@
+import pygame
+
+pygame.init()
+
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+YELLOW = (255, 255, 102)
+
+display_width = 600
+display_height = 400
+
+dis = pygame.display.set_mode((display_width, display_height))
+pygame.display.set_caption('Pong Game')
+
+game_over = False
+
+clock = pygame.time.Clock()
+
+
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+
+    dis.fill(BLACK)
+    pygame.draw.line(dis,WHITE,[300,0], [300,800], 5)
+
+    pygame.display.flip()
+
+    clock.tick(60)
+
+pygame.quit()
